377d56655c9f61da060bff55a80526efeb7cb44e
Dogukan Ulu <dogukannulu@gmail.com>
2023-06-25 23:32:43 +0300

    add schema

--- spark_streaming.py
+++ spark_streaming.py
@@ -1,6 +1,19 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,FloatType,IntegerType,StringType
 from pyspark.sql.streaming import DataStreamReader
 from pyspark.sql.functions import *
+
+schema = StructType([
+                StructField("full_name",StringType(),False),
+                StructField("gender",StringType(),False),
+                StructField("location",StringType(),False),
+                StructField("city",StringType(),False),
+                StructField("country",StringType(),False),
+                StructField("postcode",IntegerType(),False),
+                StructField("latitude",FloatType(),False),
+                StructField("longitude",FloatType(),False),
+                StructField("email",FloatType(),False)
+            ])
 
 # Create a SparkSession
 spark = SparkSession.builder \
